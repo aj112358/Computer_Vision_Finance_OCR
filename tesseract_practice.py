313d87a304e15db4c08ce298b3949ca07a520806
AJ <amarjits1@hotmail.com>
2020-08-19 21:44:04 -0600

    problem: resizing affects bbox

--- tesseract_practice.py
+++ tesseract_practice.py
@@ -7,34 +7,49 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tessera
 
 my_image = cv2.imread("sample_student.jpg")
 my_image2 = cv2.imread("sample_student_no_back_sheet.jpg")
+my_image3 = cv2.imread("handwriting_test_ADF.jpg")
 
 my_image = cv2.cvtColor(my_image, cv2.COLOR_BGR2RGB)
 
 
+image_height, image_width, _ = my_image.shape
+image_height2, image_width2, _ = my_image2.shape
+image_height3, image_width3, _ = my_image3.shape
+
+print(image_height3) # 3229
+print(image_width3) # 2479
+
 
 WIDTH = int(1366*0.7)
 HEIGHT = int(768*0.7)
+
+WIDTH3 = int(image_width3*0.25)
+HEIGHT3 = int(image_height3*0.25)
+
 my_image = cv2.resize(my_image, (WIDTH, HEIGHT)) 
 my_image2 = cv2.resize(my_image2, (WIDTH, HEIGHT))
+my_image3 = cv2.resize(my_image3, (WIDTH3, HEIGHT3))
 # cv2.namedWindow("output", cv2.WINDOW_NORMAL)
 
 
 # x = pytesseract.image_to_string(my_image, lang="eng")
 # x2 = pytesseract.image_to_string(my_image2, lang="eng")
+# x3 = pytesseract.image_to_string(my_image3, lang="eng")
 # print(x)
 # print(x2)
 
 # y = pytesseract.image_to_boxes(my_image, lang="eng")
 # y2 = pytesseract.image_to_boxes(my_image2, lang="eng")
+y3 = pytesseract.image_to_boxes(my_image3, lang="eng")
 # print(y)
 # print(y2)
 
 # z = pytesseract.image_to_data(my_image, lang="eng")
-z2 = pytesseract.image_to_data(my_image2, lang="eng")
+# z2 = pytesseract.image_to_data(my_image2, lang="eng")
+z3 = pytesseract.image_to_data(my_image3, lang="eng")
 # print(z2[0])
 
-image_height, image_width, _ = my_image.shape
-image_height2, image_width2, _ = my_image2.shape
+
 
 # for box in y.splitlines():
 #     box = box.split()
@@ -46,29 +61,59 @@ image_height2, image_width2, _ = my_image2.shape
 #     box = box.split()
 #     x,y,w,h = int(box[1]), int(box[2]), int(box[3]), int(box[4])
 #     cv2.rectangle(my_image2, (x,image_height2-y), (w,image_height2-h), (0,0,255), 1)
-#     cv2.putText(my_image2, box[0], (x,image_height2-y+25), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 2)
+    # cv2.putText(my_image2, box[0], (x,image_height2-y+25), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 2)
 
 
-for count,box in enumerate(z2.splitlines()):
-    if count == 0 :
-        continue
+# for count,box in enumerate(z2.splitlines()):
+#     if count == 0 :
+#         continue
     
+#     box = box.split()
+
+#     if len(box) == 12:
+#         x,y,w,h = int(box[6]), int(box[7]), int(box[8]), int(box[9])
+#         cv2.rectangle(my_image2, (x,y), (x+w,y+h), (0,0,255), 1)
+#         # cv2.putText(my_image2, box[11], (x,y+25), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255))
+
+#         # Printing the words that are detected
+#         if box[11] != "":
+#             print(box[11])
+
+# print(y3)
+for box in y3.splitlines():
     box = box.split()
+    print(box)
+    x,y,w,h = int(box[1]), int(box[2]), int(box[3]), int(box[4])
+    cv2.rectangle(my_image3, (x,HEIGHT3-y), (w,HEIGHT3-h), (0,0,255), 1)
+    # cv2.rectangle(my_image3, (image_height3-y,x), (image_height3-h,w), (0,0,255), 1)
+    # cv2.rectangle(my_image3, (L,HEIGHT3-T), (R,HEIGHT3-B), (0,0,255), 3)
+    # cv2.rectangle(my_image3, (x,y), (w,h), (0,0,255), 1)
+    # cv2.putText(my_image3, box[0], (x,image_height3-y+25), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 2)
 
-    if len(box) == 12:
-        x,y,w,h = int(box[6]), int(box[7]), int(box[8]), int(box[9])
-        cv2.rectangle(my_image2, (x,y), (x+w,y+h), (0,0,255), 1)
-        # cv2.putText(my_image2, box[11], (x,y+25), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255))
+cv2.rectangle(my_image3, (50,50), (100,100), (255,0,0), 3)
+# cv2.rectangle(my_image3, (0,HEIGHT3-105), (50,HEIGHT3-119), (0,255,0), 3)
 
-        # Printing the words that are detected
-        if box[11] != "":
-            print(box[11])
+# for count,box in enumerate(z3.splitlines()):
+#     if count == 0 :
+#         continue
+    
+#     box = box.split()
+
+#     if len(box) == 12:
+#         x,y,w,h = int(box[6]), int(box[7]), int(box[8]), int(box[9])
+#         cv2.rectangle(my_image3, (x,y), (x+w,y+h), (0,0,255), 1)
+#         # cv2.putText(my_image2, box[11], (x,y+25), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255))
+
+#         # Printing the words that are detected
+#         if box[11] != "":
+#             print(box[11])
 
 
 
 
 # cv2.imshow("output", my_image)
-cv2.imshow("output2", my_image2)
+# cv2.imshow("output2", my_image2)
+cv2.imshow("output3", my_image3)
 cv2.waitKey(0)
 
 
@@ -80,4 +125,4 @@ cv2.waitKey(0)
 # with open("boom2.txt", mode='a') as newfile:
 #     newfile.write(extracted_text)
 
-# print("Goodnight world")
+print("Goodnight world")
