7a1e9bd2348219c3f38e9b127b6c9249d7addfcc
AJ <amarjits1@hotmail.com>
2020-08-24 18:15:03 -0600

    finished image to data conversion

---
+++ image_to_list.py
@@ -0,0 +1,41 @@
+from sklearn.model_selection import train_test_split, GridSearchCV
+import os
+import numpy as np
+import cv2
+
+LETTERS = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+SMALL_LETTERS = [x.lower() for x in LETTERS]
+NUMBERS = ['0','1','2','3','4','5','6','7','8','9']
+SYMBOLS = ['@','$','&',',','period','-']
+
+with open("img_to_array_test.txt", "w") as newfile:
+
+    for i in range(1,36):
+        my_image = cv2.imread(fr"C:\Users\AJ\Desktop\Computer_Vision_Finance_OCR\Uppercase\A\A_{i}.jpg")
+
+        # gray_image = cv2.cvtColor(my_image, cv2.COLOR_BGR2GRAY)
+
+        # print(my_image.dtype) # uint8
+        # print(my_image.shape) # (73, 73, 3)
+
+        # cv2.imshow("original", my_image)
+        # cv2.imshow("grayscale", gray_image)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+
+        data = np.array(my_image)
+        data_list = data.tolist()
+        # print(data_list[0])
+        # print(data[0][0:3])
+        # print(data[0].shape) # 73x3 (Obviously! )
+        # print(data.shape) # (73, 73, 3)
+
+        newfile.write("A::"+str(data_list)+"\n")
+
+
+
+
+
+
+
--- svm_model.py
+++ svm_model.py
@@ -1,4 +1,4 @@
-# from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split, GridSearchCV
 # from sklearn.svm import SVC
 # from sklearn.metrics import classification_report, confusion_matrix
 import os
@@ -9,36 +9,38 @@ from matplotlib import image
 
 # Import character data (and clean???)
 
-
-my_image = cv2.imread(r"C:\Users\AJ\Desktop\Computer_Vision_Finance_OCR\Uppercase\A\A_1.jpg")
-
-gray_image = cv2.cvtColor(my_image, cv2.COLOR_BGR2GRAY)
-
-# print(my_image.dtype) # uint8
-# print(my_image.shape) # (73, 73, 3)
-
-# cv2.imshow("original", my_image)
-# cv2.imshow("grayscale", gray_image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-# plt.imshow(my_image)
-# plt.show()
-
-
-data = np.asarray(gray_image)
-print(data)
-# print(data[0][0:3])
-# print(data[0].shape) # 73x3 (Obviously! )
-# print(data.shape) # (73, 73, 3)
-
-# np.savetxt("img_to_array_test2.txt", X=data)
-# np.save("img_to_array_test2.txt", arr=data)
-
 with open("img_to_array_test.txt", "w") as newfile:
-    newfile.write("A::"+str(data))
-    # for row in range(73):
-    #     newfile.writelines(str(data[row])+",")
+
+    for i in range(1,36):
+        my_image = cv2.imread(fr"C:\Users\AJ\Desktop\Computer_Vision_Finance_OCR\Uppercase\A\A_{i}.jpg")
+
+        # gray_image = cv2.cvtColor(my_image, cv2.COLOR_BGR2GRAY)
+
+        # print(my_image.dtype) # uint8
+        # print(my_image.shape) # (73, 73, 3)
+
+        # cv2.imshow("original", my_image)
+        # cv2.imshow("grayscale", gray_image)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+        # plt.imshow(my_image)
+        # plt.show()
+
+
+        data = np.array(my_image)
+        data_list = data.tolist()
+        print(data_list[0])
+        # print(data[0][0:3])
+        # print(data[0].shape) # 73x3 (Obviously! )
+        # print(data.shape) # (73, 73, 3)
+
+        # np.savetxt("img_to_array_test2.txt", X=data)
+        # np.save("img_to_array_test2.txt", arr=data)
+
+        # newfile.write("A::"+str(data_list)+"\n")
+        # for row in range(73):
+        #     newfile.writelines(str(data[row])+",")
 
 
 
@@ -68,12 +70,10 @@ with open("img_to_array_test.txt", "w") as newfile:
 # print(os.path.dirname(full_path))
 
 
-# convert_to_numerical
-
 
 # Split the data
-# X
-# y
+X
+y
 
 
 # Create & fit the model 
